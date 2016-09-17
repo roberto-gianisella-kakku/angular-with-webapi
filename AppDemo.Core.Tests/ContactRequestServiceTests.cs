1f963b50a85094294271d873b3fa6fe1603ee7d2
Roberto <gianibob82@gmail.com>
2016-09-17 20:15:06 +1000

    Added a simple test

---
+++ AppDemo.Core.Tests/ContactRequestServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using AppDemo.Core.Abstract.Services;
+using AppDemo.Core.Concrete.Services;
+using AppDemo.Core.Data;
+using AppDemo.Core.Abstract.Repositories;
+using AppDemo.Core.Concrete.Repositories;
+using AppDemo.Core.DataTransferObjects.Request;
+using System.Threading.Tasks;
+
+namespace AppDemo.Core.Tests
+{
+    [TestClass]
+    public class ContactRequestServiceTests
+    {
+        [TestMethod]
+        public async Task CreateAsync()
+        {
+            try
+            {
+                var dbContext = InitializeEnvironment.MockContext;
+
+                IContactRequestRepository ctcRepo = new ContactRequestRepository(dbContext.Object);
+
+                IContactRequestService ctcServ = new ContactRequestService(ctcRepo);
+
+                await ctcServ.CreateAsync(new ContactRequestData
+                {
+                    Comment = "test commenr",
+                    Email = "anyone@test.com",
+                    Name = "anyone"
+                });
+
+                dbContext.Verify(m => m.SaveChangesAsync(), Times.Once);
+
+
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
---
+++ AppDemo.Core.Tests/InitializeEnvironment.cs
@@ -0,0 +1,48 @@
+﻿using AppDemo.Core.Data;
+using AppDemo.Core.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDemo.Core.Tests
+{
+    [TestClass]
+    public class InitializeEnvironment
+    {
+        public static AppDemoDbContext MockDb = null;
+
+        public static Mock<AppDemoDbContext> MockContext = null;
+
+        [AssemblyInitialize]
+        public static void InitDb(TestContext context)
+        {
+            var mockContact = new Mock<DbSet<ContactRequest>>();
+
+            var contacts = new List<ContactRequest>
+            {
+                new ContactRequest {
+                    Comment = "test commenr 1",
+                    Email = "anyon1e@test.com",
+                    Name = "an1yone"
+                }
+            }.AsQueryable();
+            
+            mockContact.As<IQueryable<ContactRequest>>().Setup(m => m.Provider).Returns(contacts.Provider);
+            mockContact.As<IQueryable<ContactRequest>>().Setup(m => m.Expression).Returns(contacts.Expression);
+            mockContact.As<IQueryable<ContactRequest>>().Setup(m => m.ElementType).Returns(contacts.ElementType);
+            mockContact.As<IQueryable<ContactRequest>>().Setup(m => m.GetEnumerator()).Returns(contacts.GetEnumerator());
+
+
+            var mockContext = new Mock<AppDemoDbContext>();
+            mockContext.Setup(c => c.ContactRequests).Returns(mockContact.Object);
+
+            MockContext = mockContext;
+            MockDb = mockContext.Object;
+        }
+    }
+}
